da7cf6ab159c9b22167661fd63e42a39621776d6
Hyemin <89293165+HyemCha@users.noreply.github.com>
2023-01-25 23:24:49 +0900

    Update 큰_수의_법칙.py

--- python_algorithm/Greedy/큰_수의_법칙.py
+++ python_algorithm/Greedy/큰_수의_법칙.py
@@ -21,3 +21,23 @@ while True:
   m -= 1
 
 print(result)
+
+
+# 다른 풀이
+# 수열; (k+1)이 반복되는
+n, m, k = map(int, sys.stdin.readline().split())
+nat = list(map(int, sys.stdin.readline().split()))
+
+nat.sort(reverse=True)
+first = nat[0]
+second = nat[1]
+
+# 가장 큰 수가 더해지는 횟수 
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+
+result = 0
+result += (count) * first
+result += (m - count) * second
+
+print(result)
