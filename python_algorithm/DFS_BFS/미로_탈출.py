cd752bc367aaa383cb2cc54a15455f523ac524e0
Hyemin <89293165+HyemCha@users.noreply.github.com>
2023-02-01 19:30:48 +0900

    Create 미로_탈출.py

---
+++ python_algorithm/DFS_BFS/미로_탈출.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+
+def bfs(x, y):
+  queue = deque()
+  queue.append((x, y))
+
+  while queue:
+    x, y = queue.leftpop()
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if nx < 0 or nx >= n or ny < 0 or ny >= m:
+        continue
+      if graph[nx][ny] == 1:
+        continue
+      graph[nx][ny] = graph[x][y] + 1
+      queue.append((nx, ny))
+
+  print(graph[n - 1][m - 1])
+
+n, m = map(int, sys.stdin.readline().split())
+
+graph = []
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+for _ in range(n):
+  graph.append(list(map(int, sys.stdin.readline().replace('\n', ''))))
+
+bfs(0, 0)
