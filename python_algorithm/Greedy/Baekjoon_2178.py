bc502640a533f4ec05a1367151b93029c9c3e1ab
HyemCha <chm6194@gmail.com>
2023-03-28 10:25:35 +0900

    Baejoob_2178.py

---
+++ python_algorithm/Greedy/Baekjoon_2178.py
@@ -0,0 +1,11 @@
+import sys
+
+def bfs(x, y):
+    pass
+
+n, m = map(int, sys.stdin.readline().split())
+
+graph = []
+for _ in range(n):
+    graph.append(list(sys.stdin.readline().replace('\n', '')))
+
