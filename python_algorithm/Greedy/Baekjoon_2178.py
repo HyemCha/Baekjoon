03ea30ce8af0a060b98300df9d1ab8ab519b4300
HyemCha <chm6194@gmail.com>
2023-03-28 20:25:23 +0900

    Baejoob_2178.py

--- python_algorithm/Greedy/Baekjoon_2178.py
+++ python_algorithm/Greedy/Baekjoon_2178.py
@@ -1,11 +1,34 @@
 import sys
+from collections import deque
+
 
 def bfs(x, y):
-    pass
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if graph[nx][ny] == '1' and visited[nx][ny] == 0:
+                visited[nx][ny] = visited[x][y] + 1
+                queue.append((nx, ny))
+
 
 n, m = map(int, sys.stdin.readline().split())
 
 graph = []
+visited = [[0]*m for _ in range(n)]
 for _ in range(n):
     graph.append(list(sys.stdin.readline().replace('\n', '')))
 
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+visited[0][0] = 1
+bfs(0, 0)
+
+print(visited[n-1][m-1])
